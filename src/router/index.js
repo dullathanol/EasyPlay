73bd9456a2fa5859605e8b4a75d58f1d245bd113
dullathanol <327533967@qq.com>
2023-03-23 11:40:31 +0800

    3/23

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHashHistory(import.meta.env.BASE_URL),  //electron
+  history: createWebHistory(import.meta.env.BASE_URL),   //web
   routes: [
     {
       path: '/',
